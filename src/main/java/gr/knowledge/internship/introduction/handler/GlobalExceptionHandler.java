c679db5c2bee5d7c5bcaff0fac8e6202aa2cf519
Panagiotis Triantafyllopoulos <pantrian04@gmail.com>
2024-03-28 11:37:54 +0200

    reorganised commits and added logger

--- src/main/java/gr/knowledge/internship/introduction/handler/GlobalExceptionHandler.java
+++ src/main/java/gr/knowledge/internship/introduction/handler/GlobalExceptionHandler.java
@@ -12,7 +12,9 @@ import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
 import gr.knowledge.internship.introduction.exception.SeasonNotFoundException;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 @Order(Ordered.HIGHEST_PRECEDENCE)
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -22,7 +24,7 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<Object> handleEntityNotFoundException(SeasonNotFoundException snf) {
 		Map<String, Object> body = new HashMap<>();
 		body.put(snf.getMessage(), snf);
-
+		log.error(snf.getMessage());
 		return new ResponseEntity<>(body, HttpStatus.NOT_FOUND);
 	}
 
@@ -30,22 +32,23 @@ public class GlobalExceptionHandler {
 	public ResponseEntity<Object> handleIllegalArgumentException(IllegalArgumentException iae) {
 		Map<String, Object> body = new HashMap<>();
 		body.put(iae.getMessage(), iae);
+		log.error(iae.getMessage());
 		return new ResponseEntity<>(body, HttpStatus.NOT_ACCEPTABLE);
-	}
-
-	@ExceptionHandler(PropertyValueException.class)
-	public ResponseEntity<Object> handleProperyValueException(PropertyValueException pve) {
-		Map<String, Object> body = new HashMap<>();
-		body.put(pve.getMessage(), pve.getCause());
-
-		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 
 	@ExceptionHandler(NullPointerException.class)
 	public ResponseEntity<Object> handleNullPointerException(NullPointerException npe) {
 		Map<String, Object> body = new HashMap<>();
 		body.put(npe.getMessage(), npe.getCause());
+		log.error(npe.getMessage());
+		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
+	}
 
+	@ExceptionHandler(PropertyValueException.class)
+	public ResponseEntity<Object> handleProperyValueException(PropertyValueException pve) {
+		Map<String, Object> body = new HashMap<>();
+		body.put(pve.getMessage(), pve.getCause());
+		log.error(pve.getMessage());
 		return new ResponseEntity<>(body, HttpStatus.INTERNAL_SERVER_ERROR);
 	}
 }
